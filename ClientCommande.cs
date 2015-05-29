86f3d9b066bd13b540feb642a2761fe8c1d1aced
fatma0000 <gazaile.fatma@yahoo.fr>
2015-05-29 23:56:07 +1200

    Create ClientCommande.cs

---
+++ ClientCommande.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMeublatexApp
+{
+    public class ClientCommande
+    {
+       public int Id_clt;
+       public int Id_cmd;
+       public int prix_total;
+    }
+}
